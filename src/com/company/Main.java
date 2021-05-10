7342a27239fe94821cd9a0cc02ca54a64e11550a
gizem <gizemsacal@gmail.com>
2021-05-10 16:17:19 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+/* package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+                   // EnumSet
+
+        Set enumSet = EnumSet.of(EnumGun.CUMARTESI, EnumGun.PAZAR);
+
+                   //EnumMap
+
+Map enumMap = new EnumMap(EnumGun.class);
+enumMap.put(EnumGun.CUMARTESI, EnumGun.CUMARTESI.getKisaltma());
+enumMap.put(EnumGun.PAZAR, EnumGun.PAZAR.getKisaltma());
+
+                    //Sonuç olarak
+ private static final int BASARILI = 1;
+private static final int BASARISIZ = 0;
+
+    }
+}
+*/
